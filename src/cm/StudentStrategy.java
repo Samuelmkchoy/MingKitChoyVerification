dc89cdd8c52584d288a5aed1a3bfeeebcaab9d91
Ming-Kit Choy <samuelmkchoy@hotmail.com>
2023-12-10 20:20:43 +0000

    Refactor: Completed StudentStrategy class. Test Pass.

--- src/cm/StudentStrategy.java
+++ src/cm/StudentStrategy.java
@@ -5,8 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StudentStrategy implements PayStrategy{
+    private static final BigDecimal REDUCTION_THRESHOLD = BigDecimal.valueOf(5.50);
+    private static final BigDecimal REDUCTION_PERCENTAGE = BigDecimal.valueOf(0.33);
+
+    @Override
     public BigDecimal applyRateReduction(BigDecimal totalCost) {
-        totalCost=BigDecimal.valueOf(6.985);
+        // Apply the Student reduction: 33% reduction on any amount above 5.50
+        if (totalCost.compareTo(REDUCTION_THRESHOLD) > 0) {
+            BigDecimal reductionAmount = totalCost.subtract(REDUCTION_THRESHOLD)
+                    .multiply(REDUCTION_PERCENTAGE);
+            return REDUCTION_THRESHOLD.add(reductionAmount);
+        }
         return totalCost;
     }
 }
