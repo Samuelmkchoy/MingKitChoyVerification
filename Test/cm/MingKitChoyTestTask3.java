ad8d090b42b1a7641d8228fe8a7f4a8019e69d80
Ming-Kit Choy <samuelmkchoy@hotmail.com>
2023-12-10 04:17:13 +0000

    RED: Created a Test For Management Charges. Test Fail.

--- Test/cm/MingKitChoyTestTask3.java
+++ Test/cm/MingKitChoyTestTask3.java
@@ -31,4 +31,21 @@ public class MingKitChoyTestTask3 {
         //STAFF: The maximum payable is 10.00 per day.
         assertEquals(new BigDecimal("10"), charge);
     }
+    @Test
+    public void ManagementCharge(){
+        CarParkKind kind = CarParkKind.MANAGEMENT;
+        BigDecimal normalRate = BigDecimal.valueOf(5);
+        BigDecimal reducedRate = BigDecimal.valueOf(2);
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+
+        normalPeriods.add(new Period(7, 24));
+        reducedPeriods.add(new Period(0, 7));
+
+        Rate rate1 = new Rate(kind, normalRate, reducedRate, normalPeriods, reducedPeriods);
+        Period periodStay = new Period(0, 1);
+        BigDecimal charge = rate1.calculate(periodStay);
+        //MANAGEMENT: minimum payable is 5.00
+        assertEquals(new BigDecimal("5"), charge);
+    }
 }
