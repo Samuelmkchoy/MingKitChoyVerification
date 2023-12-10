31d75eb8adc5de161006add61be51da268f83a62
Ming-Kit Choy <samuelmkchoy@hotmail.com>
2023-12-10 01:27:37 +0000

    Created a Test For STAFF Charges. Test Fail.

---
+++ Test/cm/MingKitChoyTestTask3.java
@@ -0,0 +1,34 @@
+package cm;
+
+import cm.CarParkKind;
+import cm.Period;
+import cm.Rate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class MingKitChoyTestTask3 {
+    @Test
+    public void StaffCharge(){
+        CarParkKind kind = CarParkKind.STAFF;
+        BigDecimal normalRate = BigDecimal.valueOf(5);
+        BigDecimal reducedRate = BigDecimal.valueOf(2);
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+
+        normalPeriods.add(new Period(7, 24));
+        reducedPeriods.add(new Period(0, 7));
+
+        Rate rate1 = new Rate(kind, normalRate, reducedRate, normalPeriods, reducedPeriods);
+        Period periodStay = new Period(0, 24);
+        BigDecimal charge = rate1.calculate(periodStay);
+        //STAFF: The maximum payable is 10.00 per day.
+        assertEquals(new BigDecimal("10"), charge);
+    }
+}
