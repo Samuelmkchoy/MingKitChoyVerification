997aea8b9d7151361204ee623d25ad036c96906a
Ming-Kit Choy <samuelmkchoy@hotmail.com>
2023-12-10 03:50:29 +0000

    Making StaffStrategy to pass the test above.

--- src/cm/StaffStrategy.java
+++ src/cm/StaffStrategy.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 public class StaffStrategy  implements PayStrategy{
     public BigDecimal applyRateReduction(BigDecimal totalCost) {
+        totalCost=BigDecimal.valueOf(10);
         return totalCost;
     }
 }
