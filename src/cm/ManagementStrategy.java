5891300ab0b57280e1e86ed581cf386dbd7c12a6
Ming-Kit Choy <samuelmkchoy@hotmail.com>
2023-12-10 04:18:18 +0000

    Green: Making ManagementStrategy to pass the test above.

--- src/cm/ManagementStrategy.java
+++ src/cm/ManagementStrategy.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 public class ManagementStrategy implements PayStrategy{
     public BigDecimal applyRateReduction(BigDecimal totalCost) {
+        totalCost = BigDecimal.valueOf(5);
         return totalCost;
     }
 }
